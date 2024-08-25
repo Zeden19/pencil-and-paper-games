a69d494e4f286d6fcdf78a9435243a350afdae5b
arjunsharma <ashar562@uwo.ca>
2024-08-24 20:07:29 -0400

    Adjust styles

--- src/Dots&Boxes/DotsAndBoxesGrid.tsx
+++ src/Dots&Boxes/DotsAndBoxesGrid.tsx
@@ -1,14 +1,17 @@
 import useDotsAndBoxes from "./stores.ts";
+import styles from "./styles.module.css";
+
 function DotsAndBoxesGrid() {
   const { grid } = useDotsAndBoxes();
   return (
-    <div style={{maxWidth: "1000px"}} className={"container text-center"}>
+    <div style={{ maxWidth: "900px" }} className={"container text-center"}>
       <div className={"row row-cols-6"}>
         {grid.map((row, rowIndex) =>
           row.map((cell, colIndex) => (
-            <div style={{ height: "125px" }}
-                 className={"col"}
-                 key={rowIndex.toString() + " " + colIndex.toString()}
+            <div
+              style={{ height: "115px" }}
+              className={`col ${rowIndex === 4 && styles.heightSmall} ${rowIndex === 0 && "mt-4"}`}
+              key={rowIndex.toString() + " " + colIndex.toString()}
             >
               <svg viewBox="0 0 80 80" width="25" height="25">
                 <circle className="circle" cx="40" cy="40" r="38" />
