d22ad5b2f213e502b12daf6a2f1eb2fe011fa4a1
arjunsharma <ashar562@uwo.ca>
2024-09-04 16:27:55 -0400

    Create mobile layout for Profile page

--- src/Account/ProfilePage.tsx
+++ src/Account/ProfilePage.tsx
@@ -32,7 +32,6 @@ function ProfilePage() {
         setUser({ ...profileData[0] });
       }
     }
-
     getUserData();
   }, [id]);
 
@@ -40,35 +39,34 @@ function ProfilePage() {
   const userGames = gameFields.map((game) => ({ ...game, value: gameMap(user, game) }));
 
   return (
-    <div className={"d-flex m-4 gap-5"}>
-      <div className={styles.leftContainer}>
-        <img
-          src={user?.avatar || ""}
-          className={"rounded-circle border border-black border-2 mb-4 w-100"}
-          alt="avatar"
-        />
-        <div className={styles.nameDescription}>
-          <h2>{user?.full_name}</h2>
-          <p className={"text-secondary fw-normal p-1"}>{user.description}</p>
+    <div className={`${styles.container}`}>
+        <div className={`${styles.leftContainer}`}>
+          <img
+            src={user?.avatar || ""}
+            className={"rounded-circle border border-black border-2 mb-4 w-100"}
+            alt="avatar"
+          />
+          <div className={styles.nameDescription}>
+            <h2>{user?.full_name}</h2>
+            <p className={"text-secondary fw-normal p-1"}>{user.description}</p>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.rightContainer}>
-        <div className={"fw-bold fs-3 mb-2"}>Games Played</div>
-        <div className={`bg-body-secondary rounded-3 px-3 w-100 pb-2`}>
-          {userGames.map((game) => (
-            <div
-              className={`d-flex justify-content-between py-2 border-bottom border-secondary-subtle ${styles.gameCard}`}
-              key={game.label}>
-              <div className={"fs-5"}>
-                {game.icon}
-                {game.label}
+        <div className={`${styles.rightContainer}`}>
+          <div className={"fw-bold fs-3 mb-2"}>Games Played</div>
+          <div className={`bg-body-secondary rounded-3 px-3 w-100 pb-2`}>
+            {userGames.map((game) => (
+              <div
+                className={`d-flex justify-content-between py-2 border-bottom border-secondary-subtle ${styles.gameCard}`}
+                key={game.label}>
+                <div className={"fs-5"}>
+                  {game.icon}
+                  {game.label}
+                </div>
+                <div className={"fs-5"}>{game.value}</div>
               </div>
-              <div className={"fs-5"}>{game.value}</div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
     </div>
   );
 }
